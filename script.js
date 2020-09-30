78ca344ef326a0fd0b064fa3c85531ef1633ac88
Chris Kalama <chriskalama96@gmail.com>
2020-09-29 23:45:01 -0500

    added universal variables to script from html

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //declare global variables. Userscore, container, high-score
+var questionBoxEl = document.getElementById("question-content-box");
+var btnAEl = document.getElementById("btnA");
+var btnBEl = document.getElementById("btnB");
+var btnCEl = document.getElementById("btnC");
+
+
+
+
 
 //What I want to see on the page when it first loads. 
 
@@ -10,3 +18,33 @@
 
 //Array Starts, for loop identify the var, i=x, x++ use ===. Some if else statements to determine if User is right. 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Extra Help
+
+var score = 0;
+var quizContainer= document.querySelector('quiz');
+var resultsContainer = document.querySelector('submit');
+
+var questions = [
+    {
+        question
+        choices
+        answer 
+    }
+]
+for (let index = 0; index < question.length; index++) {
+    const element = array[index];
+    
+}
