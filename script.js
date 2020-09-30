95d9e98f0dd831dc3f05467efc75cbe920feeb84
Chris Kalama <chriskalama96@gmail.com>
2020-09-29 23:09:04 -0500

    Built out HTML page with header, logo, question rules in a p tag, built two containers for header and text-content box. Added css style to content, header. Added 3 buttons, styled them in css with margin and font-size

---
+++ script.js
@@ -0,0 +1,12 @@
+//declare global variables. Userscore, container, high-score
+
+//What I want to see on the page when it first loads. 
+
+//Use Toggle Logic to start quiz. 
+
+// timer to start. setInterval(someFunction, seconds*1000)
+
+// Declare a function for question array to begin.  Declare [array of objects] - questions, options, and/or booleans
+
+//Array Starts, for loop identify the var, i=x, x++ use ===. Some if else statements to determine if User is right. 
+
